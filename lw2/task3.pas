db98eb70284db250a37bd8773b4b7632ffe62b6f
igormayshanov <79751645+igormayshanov@users.noreply.github.com>
2021-02-27 20:26:43 +0300

    Add files via upload

---
+++ lw2/task3.pas
@@ -0,0 +1,15 @@
+{Реализуйте программу, которая выводит приветствие 'Hello dear, <name>!'. 
+Имя должно передаваться в параметре name QUERY_STRING, в случае когда параметра name нет, должно выводиться 'Hello Anonymous!'. 
+В данном задании можно считать что параметр name стоит на первом месте, либо его нет. Можно использовать функции POS COPY для работы со строками}
+PROGRAM GreetUser(INPUT, OUTPUT);
+uses GPC;
+
+BEGIN
+  WRITELN('Content-type: text/plain');
+  WRITELN;
+  IF POS('name', GetEnv('QUERY_STRING')) > 0
+  THEN
+    WRITELN('Hello dear, ', COPY(GetEnv('QUERY_STRING'), POS('=', GetEnv('QUERY_STRING')) + 1, 20))
+  ELSE
+    WRITELN('Hello Anonymous!')  
+END.  
---
+++ lw2/task4.pas
@@ -0,0 +1,40 @@
+PROGRAM WorkWithQueryString(INPUT, OUTPUT);
+USES GPC;
+
+FUNCTION GetQueryStringParameter(Key: STRING): STRING;
+VAR
+  QS: STRING;
+  PosKey, PosAmp: INTEGER; 
+BEGIN
+  QS := GetEnv('QUERY_STRING');
+  PosKey := POS(Key, QS);
+  PosAmp := POS('&', QS);
+  IF PosAmp = 0
+  THEN
+    GetQueryStringParameter := COPY(QS, PosKey + LENGTH(Key) + 1, LENGTH(QS));
+  IF PosKey < PosAmp
+  THEN
+    GetQueryStringParameter := COPY(QS, PosKey + Length(Key) + 1, PosAmp - LENGTH(Key) - PosKey - 1);
+  IF PosKey > PosAmp
+  THEN
+    BEGIN    
+      QS := COPY(QS, PosAmp + (PosKey - PosAmp), LENGTH(QS));
+      PosKey := POS(Key, QS);
+      PosAmp := POS('&', QS);
+      IF PosAmp = 0
+      THEN
+        GetQueryStringParameter := COPY(QS, PosKey + LENGTH(Key) + 1, LENGTH(QS));
+      IF PosKey < PosAmp
+      THEN
+        GetQueryStringParameter := COPY(QS, PosKey + Length(Key) + 1, PosAmp - LENGTH(Key) - PosKey - 1);
+    END;  
+END;
+  
+BEGIN {WorkWithQueryString}
+  WRITELN('Content-type: text/plain');
+  WRITELN;
+  WRITELN('First Name: ', GetQueryStringParameter('first_name'));
+  WRITELN('Last Name: ', GetQueryStringParameter('last_name'));
+  WRITELN('Age: ', GetQueryStringParameter('age'))
+END. {WorkWithQueryString}
+
